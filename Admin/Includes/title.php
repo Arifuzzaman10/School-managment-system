24d59921d5d2779de30a799bd655935cabcf014b
Arifuzzaman Raihan <92682227+Arifuzzaman10@users.noreply.github.com>
2022-11-29 00:23:24 +0600

    System

---
+++ Admin/Includes/title.php
@@ -0,0 +1,1 @@
+  <title>BUHS - Dashboard</title>
---
+++ ClassTeacher/Includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by "Arifuzzaman Raihan SSC Batch 2016"
+              STUDENT ATTENDANCE MANAGEMENT SYSTEM "BUHS"
+            </span>
+          </div>
+        </div>
+      </footer>
